fb21445de11c68254527a958a42f8948d786933d
Micmichael1 <lmicmichael21@gmail.com>
2024-03-25 12:01:37 +0700

    Buat File Baru

---
+++ function.js
@@ -0,0 +1,4 @@
+function addition(pertama,kedua){
+    return pertama+kedua;
+}
+console.log(addition(1,2));
--- hello_world.js
+++ hello_world.js
@@ -1,6 +1,1 @@
-function addition(pertama,kedua){
-    return pertama+kedua;
-}
-var x = 1;
-var y = "michael"
-console.log(addition(1,2));
+console.log("Hello World");
